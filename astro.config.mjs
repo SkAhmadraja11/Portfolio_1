861686b14e7db494430f3077c863dab86a3d2703
SkAhmadraja11 <2300032147@kluniversity.in>
2026-02-15 22:25:04 +0530

    build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
+import node from '@astrojs/node';
 
+// https://astro.build/config
 export default defineConfig({
-  output: 'static',
-  site: 'https://example.com',
-  integrations: [],
-})
+  output: 'server',
+  adapter: node({ mode: 'standalone' }),
+});
