1718d5ea347e3c7ea06928214279a27d6688dfc7
SkAhmadraja11 <2300032147@kluniversity.in>
2026-02-17 22:16:22 +0530

    build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: node({ mode: 'standalone' }),
+  site: 'https://SkAhmadraja11.github.io',
+  base: '/Portfolio_1',
+  output: 'static',
 });
